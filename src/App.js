88ed1644ca16d319fdcdac8c3682eddf0558b14a
Gabriel <gabe.jara@gmail.com>
2023-03-27 21:57:09 +0200

    början av registration

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { useForm } from 'react-hook-form';
 
-function App() {
+const RegistrationForm = () => {
+  const { register, handleSubmit, formState: { errors } } = useForm();
+
+  const onSubmit = data => {
+    console.log(data);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <label htmlFor="firstName">First Name:</label>
+      <input {...register('firstName', { required: 'First Name is required' })} />
+      {errors.firstName && <p>{errors.firstName.message}</p>}
 
-export default App;
+      
